742a8d45408c523da7311d1e6b8baa9622457abd
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-10-22 07:11:32 +0200

    Merge the contexts.

--- kube_merge/mainapp.py
+++ kube_merge/mainapp.py
@@ -42,24 +42,24 @@ def read_yaml_file(file_name, validate) -> Dict:
 
 def merge_kube_context(
         merged_yaml: Dict,
-        new_content: Dict,
-        *,
-        merge_users: bool,
-        merge_clusters: bool) -> None:
+        new_content: Dict) -> None:
     """
     Merges the data from the other context
     """
-    if merge_clusters:
-        for cluster in new_content["clusters"]:
-            print(green("Merging cluster"),
-                  green(cluster["name"], bold=True))
-            merged_yaml["clusters"].append(cluster)
+    for cluster in new_content["clusters"]:
+        print(green("Merging cluster"),
+              green(cluster["name"], bold=True))
+        merged_yaml["clusters"].append(cluster)
 
-    if merge_users:
-        for user in new_content["users"]:
-            print(green("Merging user"),
-                  green(user["name"], bold=True))
-            merged_yaml["users"].append(user)
+    for user in new_content["users"]:
+        print(green("Merging user"),
+              green(user["name"], bold=True))
+        merged_yaml["users"].append(user)
+
+    for context in new_content["contexts"]:
+        print(green("Merging context"),
+              green(context["name"], bold=True))
+        merged_yaml["contexts"].append(user)
 
 
 def write_yaml_file(file_name, content) -> None:
@@ -80,29 +80,15 @@ def write_yaml_file(file_name, content) -> None:
               help="Validates if the config was saved with --raw.")
 @click.option("--output-context",
               default=DEFAULT_KUBE_CONFIG,
-              help=f"Specify the .kube/config filename. Defaults to {DEFAULT_KUBE_CONFIG}.")
-@click.option("--only-users",
-              is_flag=True,
-              help="Get only the users.")
-@click.option("--only-clusters",
-              is_flag=True,
-              help="Get only the clusters.")
+              help=f"Specify the .kube/config filename. Defaults to "
+                   f"{DEFAULT_KUBE_CONFIG}.")
 def main(output_context: str,
          contexts_to_merge: List[str],
-         validate: bool,
-         only_users: bool,
-         only_clusters: bool) -> None:
+         validate: bool) -> None:
     if not contexts_to_merge:
         print(red("Paths to the contexts to be merged was not provided."
                   " Pass it as an argument."))
         sys.exit(1)
-
-    if only_users and only_clusters:
-        print(red("Both only users, and only clusters ware passed"))
-        sys.exit(2)
-
-    merge_users = not only_users and not only_clusters or only_users
-    merge_clusters = not only_users and not only_clusters or only_clusters
 
     merged_yaml = read_yaml_file(output_context, validate)
 
@@ -110,9 +96,7 @@ def main(output_context: str,
         new_context = read_yaml_file(f, validate)
         merge_kube_context(
                 merged_yaml,
-                new_context,
-                merge_users=merge_users,
-                merge_clusters=merge_clusters)
+                new_context)
 
     write_yaml_file(output_context, merged_yaml)
 
