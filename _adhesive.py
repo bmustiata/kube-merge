866176a41eb5c451e1f003d37abf21f85b374d0f
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-10-22 05:15:03 +0200

    Adhesive runs build end-to-end

--- _adhesive.py
+++ _adhesive.py
@@ -2,7 +2,11 @@ import germanium_py_exe  # type: ignore
 
 
 germanium_py_exe.pipeline({
+    "run_mypy": True,
+    "run_flake8": True,
+    "repo": "git@github.com:bmustiata/kube-merge.git",
     "binaries": {
+        "name": "Python 3.7 on Linux x64",
         "platform": "python:3.7",
         "publish_pypi": "sdist",
     }
